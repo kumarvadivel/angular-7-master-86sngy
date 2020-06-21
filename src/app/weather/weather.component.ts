1ea28bf9dd755e2d5634354f56826c84b73f7aae
kumarvadivel <kumarvadivel9443493672@gmail.com>
2020-06-21 17:32:44 +0530

    1 file modified

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit ,OnDestroy,Input} from '@angular/core';
+import { Component, OnInit ,OnDestroy,Input,OnChanges} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Http , Response } from '@angular/http';
 
@@ -7,9 +7,9 @@ import { Http , Response } from '@angular/http';
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.css']
 })
-export class WeatherComponent implements OnInit {
+export class WeatherComponent implements OnInit,OnChanges {
   @Input() compno:any;
-  
+  f:any;
   weathertoggler:boolean=false;
   iconlink
   searchtext:string="search";
@@ -23,11 +23,13 @@ export class WeatherComponent implements OnInit {
     this.searchtext=event.target.value;
     this.weathertoggler=false;
     //let link="http://api.openweathermap.org/data/2.5/weather?q="+this.searchtext+"&appid=5269c981512a19f891f0f5fcaaed64ba";
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+this.searchtext+'&appid=ff1bc4683fc7325e9c57e586c20cc03e')
+    this.f=setInterval(()=>{
+      fetch('https://api.openweathermap.org/data/2.5/weather?q='+this.searchtext+'&appid=ff1bc4683fc7325e9c57e586c20cc03e')
     .then(response=>response.json())
     .then(data=>{this.weatherdata=data
+    console.log(data)
     localStorage.setItem(this.searchtext,JSON.stringify(this.weatherdata))
-    })
+    })},3000)
     //this.weathertoggler=true;
    
     
@@ -48,6 +50,12 @@ export class WeatherComponent implements OnInit {
       this.weatherdata={"coord":{"lon":"N/A","lat":"N/A"},"weather":[{"id":"N/A","main":"N/A","description":"N/A","icon":null}],"base":"N/A","main":{"temp":"N/A","feels_like":"N/A","temp_min":"N/A","temp_max":"N/A","pressure":"N/A","humidity":0},"visibility":0,"wind":{"speed":"N/A","deg":"N/A"},"clouds":{"all":"N/A"},"dt":"N/A","sys":{"type":"N/A","id":"N/A","country":"N/A","sunrise":"N/A","sunset":"N/A"},"timezone":"N/A","id":"N/A","name":"N/A","cod":225};
     }
     
+  }
+  ngOnChanges(){
+    if(this.f){
+      clearInterval(this.f)
+    }
+
   }
   ngOnDestroy(){
     
