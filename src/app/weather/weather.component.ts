170dcfb31b291ba600d374a722d2b8dea24a02e8
kumarvadivel <kumarvadivel9443493672@gmail.com>
2020-06-21 13:52:51 +0530

    2 files modified

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,OnDestroy} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Http , Response } from '@angular/http';
 
@@ -22,13 +22,18 @@ export class WeatherComponent implements OnInit {
     //let link="http://api.openweathermap.org/data/2.5/weather?q="+this.searchtext+"&appid=5269c981512a19f891f0f5fcaaed64ba";
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+this.searchtext+'&appid=ff1bc4683fc7325e9c57e586c20cc03e')
     .then(response=>response.json())
-    .then(data=>{this.weatherdata=data})
+    .then(data=>{this.weatherdata=data
+    localStorage.setItem(this.searchtext,JSON.stringify(this.weatherdata))})
     //this.weathertoggler=true;
+   
     
   }
   constructor(private  http: Http) { }
 
   ngOnInit() {
+  }
+  ngOnDestroy(){
+   
   }
 
 }
