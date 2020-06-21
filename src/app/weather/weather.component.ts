cf89faa740ae8a7920ed75e081e08e8c00b887ea
kumarvadivel <kumarvadivel9443493672@gmail.com>
2020-06-21 10:25:26 +0530

    3 files modified

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-weather',
@@ -6,7 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
+  weathertoggler:boolean=false;
+  searchtext:string="search";
+  toggler(){
+    this.weathertoggler=true;
+  }
+  searchweather($event){
+    this.searchtext=event.target.value;
+    this.weathertoggler=false;
+    let link="http://api.openweathermap.org/data/2.5/weather?q="+this.searchtext+"&APPID=5269c981512a19f891f0f5fcaaed64ba"
+    let httpclient:HttpClient;
+   fetch(link).then(data=>{
+     console.log(data)
+   })
 
+  }
   constructor() { }
 
   ngOnInit() {
