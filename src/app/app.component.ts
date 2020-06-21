e174a61585275d553b6e00ea761654563f3bd3e5
kumarvadivel <kumarvadivel9443493672@gmail.com>
2020-06-21 09:51:04 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,5 @@ import sdk from '@stackblitz/sdk';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
-  vmPlaceholder: any; 
-  ngAfterViewInit() {
-    sdk.embedProjectId('myDiv','angular-7-node').then(vm => {
-      this.vmPlaceholder = vm;
-    });
-  }
-
-  editFiles(): void {
-      var obj = {};
-    obj['src/app/app.component.html'] = '<h1>This was updated      programmatically!</h1>';
-    obj['test.html'] = 'Random file';
-    this.vmPlaceholder.applyFsDiff({
-          create: obj,
-          destroy: ['']
-        });    
-  }
+  
 }
