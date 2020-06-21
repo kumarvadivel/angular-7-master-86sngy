0ed8e470683e9e64eb10a5eef8fb8e9cb1fc0b73
kumarvadivel <kumarvadivel9443493672@gmail.com>
2020-06-21 12:39:17 +0530

    2 files modified

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -10,14 +10,16 @@ import { Http , Response } from '@angular/http';
 export class WeatherComponent implements OnInit {
   weathertoggler:boolean=false;
   searchtext:string="search";
-  weatherdata:any;
+  weatherdata:object=
+    {"coord":{"lon":"N/A","lat":"N/A"},"weather":[{"id":"N/A","main":"N/A","description":"N/A","icon":"N/A"}],"base":"N/A","main":{"temp":"N/A","feels_like":"N/A","temp_min":"N/A","temp_max":"N/A","pressure":"N/A","humidity":0},"visibility":0,"wind":{"speed":"N/A","deg":"N/A"},"clouds":{"all":"N/A"},"dt":"N/A","sys":{"type":"N/A","id":"N/A","country":"N/A","sunrise":"N/A","sunset":"N/A"},"timezone":"N/A","id":"N/A","name":"N/A","cod":"N/A"}
+  ;
   toggler(){
     this.weathertoggler=true;
   }
   searchweather($event){
     this.searchtext=event.target.value;
     this.weathertoggler=false;
-    let link="http://api.openweathermap.org/data/2.5/weather?q="+this.searchtext+"&appid=5269c981512a19f891f0f5fcaaed64ba";
+    //let link="http://api.openweathermap.org/data/2.5/weather?q="+this.searchtext+"&appid=5269c981512a19f891f0f5fcaaed64ba";
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+this.searchtext+'&appid=ff1bc4683fc7325e9c57e586c20cc03e')
     .then(response=>response.json())
     .then(data=>{this.weatherdata=data})
