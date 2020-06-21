71c74422d3d2dea53ce9a0b2406e5589a79f10ab
kumarvadivel <kumarvadivel9443493672@gmail.com>
2020-06-21 12:08:25 +0530

    2 files modified

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Http , Response } from '@angular/http';
 
 @Component({
   selector: 'app-weather',
@@ -9,26 +10,21 @@ import { HttpClient } from '@angular/common/http';
 export class WeatherComponent implements OnInit {
   weathertoggler:boolean=false;
   searchtext:string="search";
+  weatherdata:any;
   toggler(){
     this.weathertoggler=true;
   }
   searchweather($event){
     this.searchtext=event.target.value;
     this.weathertoggler=false;
-    
-    this.httpClient.get("http://api.openweathermap.org/data/2.5/weather?q="+this.searchtext+"&APPID=5269c981512a19f891f0f5fcaaed64ba")
-    .subscribe(
-      (res: Response) => 
-        {
-          const searchResult = res.json();  
-          console.log(searchResult);
-          
-          //console.log(this.imageSearch.owner.avatar_url);
-        }
-    );
+    let link="http://api.openweathermap.org/data/2.5/weather?q="+this.searchtext+"&appid=5269c981512a19f891f0f5fcaaed64ba";
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+this.searchtext+'&appid=ff1bc4683fc7325e9c57e586c20cc03e')
+    .then(response=>response.json())
+    .then(data=>{this.weatherdata=data})
+    //this.weathertoggler=true;
     
   }
-  constructor(private httpClient:HttpClient) { }
+  constructor(private  http: Http) { }
 
   ngOnInit() {
   }
